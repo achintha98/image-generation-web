5d17abc2c036d1e37744742197faf7f1afb07f67
achintha98 <kalanaachintha@gmail.com>
2025-07-03 20:17:51 +0530

    Changed Generate Image name convension

--- components/Camera.tsx
+++ components/Camera.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { useAuth } from "@clerk/nextjs";
 import React, { useEffect, useState } from "react";
 import axios from "axios";
--- components/generateImage.tsx
+++ components/generateImage.tsx
@@ -1,7 +1,69 @@
+"use client";
+
 import React from "react";
+import { useAuth } from "@clerk/nextjs";
+import { useState } from "react";
+import { Button } from "./ui/button";
+import axios from "axios";
+import { BACKEND_URL } from "@/app/config";
+import { Sparkles } from "lucide-react";
 
 const GenerateImage = () => {
-  return <div>test generate</div>;
+  const [prompt, setPrompt] = useState("");
+  const [selectedModel, setSelectedModel] = useState<string>();
+  const [isGenerating, setIsGenerating] = useState(false);
+  const { getToken } = useAuth();
+
+  const handleGenerate = async () => {
+    if (!prompt || !selectedModel) return;
+
+    setIsGenerating(true);
+    try {
+      const token = await getToken();
+      await axios.post(
+        `${BACKEND_URL}/ai/generate`,
+        {
+          prompt,
+          modelId: selectedModel,
+          num: 1,
+        },
+        {
+          headers: { Authorization: `Bearer ${token}` },
+        }
+      );
+      console.log("Image generation started!");
+      setPrompt("");
+    } catch (error) {
+      console.log(error, "Failed to generate image");
+    } finally {
+      setIsGenerating(false);
+    }
+  };
+  return (
+    <div className="space-y-6">
+      <div className="space-y-4">
+        <div className="flex justify-end pt-4">
+          <div className="flex items-center gap-2 w-full max-w-xl">
+            <input
+              type="text"
+              value={prompt}
+              onChange={(e) => setPrompt(e.target.value)}
+              placeholder="Enter your prompt..."
+              className="flex-1 border border-gray-300 rounded-md px-4 py-2"
+            />
+            <Button
+              onClick={handleGenerate}
+              disabled={isGenerating || !prompt || !selectedModel}
+              variant={"outline"}
+              className="relative z-20 cursor-pointer"
+            >
+              Generate Image <Sparkles size={24} />
+            </Button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default GenerateImage;
